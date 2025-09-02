dc4236fe8fc0e68a450b928d63c0a375629e7825
Julio Rodríguez <78330050+jerodriguezb@users.noreply.github.com>
2025-09-02 19:24:29 -0300

    feat(frontend): add code column and subtotal formatting

--- frontend/src/components/ComandaPrintView.jsx
+++ frontend/src/components/ComandaPrintView.jsx
@@ -9,19 +9,23 @@ import {
   TableCell,
 } from '@mui/material';
 
-export default function ComandaPrintView({ items = [] }) {
+export default function ComandaPrintView({ items = [], showTotal = true }) {
   const currencyFormatter = new Intl.NumberFormat('es-AR', {
     style: 'currency',
     currency: 'ARS',
   });
 
-  const total = items.reduce((sum, item) => sum + item.precio * item.cantidad, 0);
+  const total = items.reduce(
+    (sum, item) => sum + Number(item.precio) * Number(item.cantidad),
+    0,
+  );
 
   return (
     <Box>
       <Table size="small">
         <TableHead>
           <TableRow>
+            <TableCell>Código</TableCell>
             <TableCell>Producto</TableCell>
             <TableCell align="right">Precio</TableCell>
             <TableCell align="right">Cantidad</TableCell>
@@ -29,28 +33,34 @@ export default function ComandaPrintView({ items = [] }) {
           </TableRow>
         </TableHead>
         <TableBody>
-          {items.map((item) => (
-            <TableRow key={item.codprod}>
-              <TableCell>{item.descripcion || item.codprod}</TableCell>
-              <TableCell align="right">
-                {currencyFormatter.format(item.precio)}
+          {items.map((item) => {
+            const subtotal = Number(item.precio) * Number(item.cantidad);
+            return (
+              <TableRow key={item.codigo ?? item.codprod}>
+                <TableCell>{item.codigo ?? item.codprod}</TableCell>
+                <TableCell>{item.descripcion || item.codprod}</TableCell>
+                <TableCell align="right">
+                  {currencyFormatter.format(Number(item.precio))}
+                </TableCell>
+                <TableCell align="right">{item.cantidad}</TableCell>
+                <TableCell align="right">
+                  {currencyFormatter.format(subtotal)}
+                </TableCell>
+              </TableRow>
+            );
+          })}
+          {showTotal && (
+            <TableRow>
+              <TableCell colSpan={4} align="right">
+                <Typography variant="subtitle2">Total</Typography>
               </TableCell>
-              <TableCell align="right">{item.cantidad}</TableCell>
               <TableCell align="right">
-                {currencyFormatter.format(item.precio * item.cantidad)}
+                <Typography variant="subtitle2">
+                  {currencyFormatter.format(total)}
+                </Typography>
               </TableCell>
             </TableRow>
-          ))}
-          <TableRow>
-            <TableCell colSpan={3} align="right">
-              <Typography variant="subtitle2">Total</Typography>
-            </TableCell>
-            <TableCell align="right">
-              <Typography variant="subtitle2">
-                {currencyFormatter.format(total)}
-              </Typography>
-            </TableCell>
-          </TableRow>
+          )}
         </TableBody>
       </Table>
     </Box>
